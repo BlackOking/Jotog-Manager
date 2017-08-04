5ffdc9577d0b9cdfb1f7e7f7f5c295c0cf6288ad
BlackOking <theblackoking@gmail.com>
2017-08-04 17:28:10 +0200

    Modification texte fermeture projet

--- Jotog Manager/PrincipaleForm.Designer.cs
+++ Jotog Manager/PrincipaleForm.Designer.cs
@@ -79,7 +79,7 @@
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(150, 23);
             this.button4.TabIndex = 4;
-            this.button4.Text = "Fermer";
+            this.button4.Text = "Fermer le projet";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
