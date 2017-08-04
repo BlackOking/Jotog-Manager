ea8baa4d8a0a4cb1818e239c0559aa7d76befc99
BlackOking <theblackoking@gmail.com>
2017-08-04 17:33:08 +0200

    Organisation du code de PrincipalForm

--- Jotog Manager/PrincipaleForm.cs
+++ Jotog Manager/PrincipaleForm.cs
@@ -31,6 +31,8 @@ namespace Jotog_Manager
             MyForm.Show();
         }
 
+        //Click sur le bouton "Projet Esthon"
+
         private void button1_Click(object sender, EventArgs e)
         {
             tableauDeBord = "Esthon";
@@ -46,6 +48,24 @@ namespace Jotog_Manager
             MyForm.BringToFront();
             MyForm.Show();
         }
+
+        //Click sur le bouton "HearthLuria"
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            HearthLuriaForm MyForm = new HearthLuriaForm();
+            MyForm.TopLevel = false;
+            MyForm.FormBorderStyle = FormBorderStyle.None;
+            MyForm.Parent = panel1;
+            MyForm.Top = 0;
+            MyForm.Left = 0;
+            MyForm.Size = panel1.ClientSize;
+            MyForm.Dock = DockStyle.Fill;
+            MyForm.BringToFront();
+            MyForm.Show();
+        }
+
+        //Click sur le bouton "Luria"
 
         private void button3_Click(object sender, EventArgs e)
         {
@@ -63,6 +83,8 @@ namespace Jotog_Manager
             MyForm.Show();
         }
 
+        //Click sur le bouton de fermeture de projet
+
         private void button4_Click(object sender, EventArgs e)
         {
 
@@ -78,18 +100,5 @@ namespace Jotog_Manager
             MyForm.Show();
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            HearthLuriaForm MyForm = new HearthLuriaForm();
-            MyForm.TopLevel = false;
-            MyForm.FormBorderStyle = FormBorderStyle.None;
-            MyForm.Parent = panel1;
-            MyForm.Top = 0;
-            MyForm.Left = 0;
-            MyForm.Size = panel1.ClientSize;
-            MyForm.Dock = DockStyle.Fill;
-            MyForm.BringToFront();
-            MyForm.Show();
-        }
     }
 }
