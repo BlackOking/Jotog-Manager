797db01ffbb55a62501f6cfb93d5e4cc4d6b85ce
BlackOking <theblackoking@gmail.com>
2017-08-17 22:48:45 +0200

    Ajout du form Garry

--- Alpagua Manager/Properties/Resources.Designer.cs
+++ Alpagua Manager/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace Alpagua_Manager.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Recherche une ressource localisée de type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap GarryLogo {
+            get {
+                object obj = ResourceManager.GetObject("GarryLogo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
