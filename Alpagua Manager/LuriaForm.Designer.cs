983bc5130b4a2489d99ffe338782fb777b9b309e
BlackOking <theblackoking@gmail.com>
2017-08-18 13:25:23 +0200

    Ajout du bouton compte rendu sur LuriaForm

--- Alpagua Manager/LuriaForm.Designer.cs
+++ Alpagua Manager/LuriaForm.Designer.cs
@@ -35,6 +35,7 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.process1 = new System.Diagnostics.Process();
+            this.button1 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -110,11 +111,21 @@
             this.process1.StartInfo.UserName = "";
             this.process1.SynchronizingObject = this;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Compte rendu";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // LuriaForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(749, 497);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
             this.Name = "LuriaForm";
@@ -135,5 +146,6 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Diagnostics.Process process1;
+        private System.Windows.Forms.Button button1;
     }
 }
