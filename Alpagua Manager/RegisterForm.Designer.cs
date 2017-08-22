87763deeaf73979d1b3033683d2e452792ca02d2
BlackOking <theblackoking@gmail.com>
2017-08-22 12:49:17 +0200

    Ajout du système de création de compte

---
+++ Alpagua Manager/RegisterForm.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Alpagua_Manager
+{
+    partial class RegisterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RegisterForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.usernametBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.passwordtBox = new System.Windows.Forms.TextBox();
+            this.cpasswordtBox = new System.Windows.Forms.TextBox();
+            this.registerButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.nametBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MercedesSerial", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(53, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(282, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Création de compte";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nom d\'utilisateur :";
+            // 
+            // usernametBox
+            // 
+            this.usernametBox.Location = new System.Drawing.Point(108, 85);
+            this.usernametBox.Name = "usernametBox";
+            this.usernametBox.Size = new System.Drawing.Size(273, 20);
+            this.usernametBox.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Mot de passe :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 159);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(134, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Confirmer le mot de passe :";
+            // 
+            // passwordtBox
+            // 
+            this.passwordtBox.Location = new System.Drawing.Point(95, 121);
+            this.passwordtBox.Name = "passwordtBox";
+            this.passwordtBox.Size = new System.Drawing.Size(286, 20);
+            this.passwordtBox.TabIndex = 5;
+            this.passwordtBox.UseSystemPasswordChar = true;
+            // 
+            // cpasswordtBox
+            // 
+            this.cpasswordtBox.Location = new System.Drawing.Point(152, 156);
+            this.cpasswordtBox.Name = "cpasswordtBox";
+            this.cpasswordtBox.Size = new System.Drawing.Size(229, 20);
+            this.cpasswordtBox.TabIndex = 6;
+            this.cpasswordtBox.UseSystemPasswordChar = true;
+            // 
+            // registerButton
+            // 
+            this.registerButton.Location = new System.Drawing.Point(61, 231);
+            this.registerButton.Name = "registerButton";
+            this.registerButton.Size = new System.Drawing.Size(102, 39);
+            this.registerButton.TabIndex = 8;
+            this.registerButton.Text = "S\'enregistrer";
+            this.registerButton.UseVisualStyleBackColor = true;
+            this.registerButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(234, 231);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(101, 39);
+            this.cancelButton.TabIndex = 9;
+            this.cancelButton.Text = "Annuler";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 192);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Ton prénom :";
+            // 
+            // nametBox
+            // 
+            this.nametBox.Location = new System.Drawing.Point(88, 189);
+            this.nametBox.Name = "nametBox";
+            this.nametBox.Size = new System.Drawing.Size(293, 20);
+            this.nametBox.TabIndex = 7;
+            // 
+            // RegisterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.ClientSize = new System.Drawing.Size(393, 296);
+            this.Controls.Add(this.nametBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.registerButton);
+            this.Controls.Add(this.cpasswordtBox);
+            this.Controls.Add(this.passwordtBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.usernametBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "RegisterForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Création de compte";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox usernametBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox passwordtBox;
+        private System.Windows.Forms.TextBox cpasswordtBox;
+        private System.Windows.Forms.Button registerButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox nametBox;
+    }
+}
---
+++ Alpagua Manager/RegisterForm.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Alpagua_Manager
+{
+    public partial class RegisterForm : Form
+    {
+        private MySqlConnection connexion;
+        private string server;
+        private string database;
+        private string uid;
+        private string passwordsql;
+
+        public RegisterForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            server = "mysql-alpagua.alwaysdata.net";
+            database = "alpagua_manager";
+            uid = "alpagua";
+            passwordsql = "JotogManager-2017";
+
+            string connString;
+            connString = $"SERVER={server};DATABASE={database};UID={uid};PASSWORD={passwordsql};";
+
+            connexion = new MySqlConnection(connString);
+
+            if (usernametBox.Text == "" || passwordtBox.Text == "" || cpasswordtBox.Text == "" || nametBox.Text == "")
+            {
+                MessageBox.Show("Veuillez remplir l'ensemble des champs");
+            }
+            else if (passwordtBox.Text != cpasswordtBox.Text)
+            {
+                MessageBox.Show("Le mot de passe et la confirmation de mot de passe ne sont pas identiques. Veuillez vérifier votre mot de passe.");
+            }
+            else
+            {
+
+                ChargementForm chargForm = new ChargementForm();
+                chargForm.Show();
+
+                using (connexion = new MySqlConnection($"SERVER={server};DATABASE={database};UID={uid};PASSWORD={passwordsql};"))
+                {
+
+                    using (MySqlCommand cmd = new MySqlCommand("", connexion))
+                    {
+                        connexion.Open();
+                        MySqlCommand command = new MySqlCommand("INSERT INTO users(Username,Password,Name) VALUES('" + usernametBox.Text + "',MD5('" + passwordtBox.Text + "'),'" + nametBox.Text + "')", connexion);
+                        command.ExecuteNonQuery();
+                        MessageBox.Show("Votre compte à bien été créé, veuillez maintenant vous connecter");
+                        this.Close();
+                        }
+                    }
+                }
+            }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+ }
